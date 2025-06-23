1734b985820a94fe748b3f0174c6a1ef786974e7
shauray8 <shauray9@gmail.com>
2025-06-23 18:00:35 +0530

    solve for relative imports

---
+++ grat/__init__.py
@@ -0,0 +1,1 @@
+from grat_flux_attn_processor import init_local_mask_flex, GratFluxAttnProcessor 
--- inference_flux.py
+++ inference_flux.py
@@ -1,3 +1,4 @@
+from grat import GratFluxAttnProcessor, init_local_mask_flex
 from diffusers import FluxPipeline
 import torch 
 
@@ -19,12 +20,11 @@ mask = init_local_mask_flex(
 attn_processors = {}
 for k, v in pipe.transformer.attn_processors.items():
     if "single_transformer_blocks" in k:
-        print(k)
         attn_processors[k] = v
     else:
-        attn_processors[k] = EnhancedFluxAttnProcessor(
+        attn_processors[k] = GratFluxAttnProcessor(
             mask, height // 16, width // 16, group_h, group_w, text_length
         )
 pipe.transformer.set_attn_processor(attn_processors)
 
-output = pipe(prompt=prompt, height=height, width=width, num_inference_steps=28, max_sequence_length=512).images[0]
+output = pipe(prompt=prompt, height=height, width=width, num_inference_steps=28, max_sequence_length=text_length).images[0]
